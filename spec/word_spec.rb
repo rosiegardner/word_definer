17f045a0569dabe5867fed8b8a1cc436b668660c
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-05 18:53:46 -0700

    wrote test for class method .clear - Expect FAIL

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,12 +2,6 @@ require('rspec')
 require('word')
 
 describe '#Word' do
-  describe('.all') do
-    it("returns an empty array when there are no words") do
-      expect(Word.all).to(eq([]))
-    end
-  end
-
   describe('#save') do
     it("saves a word") do
       word = Word.new("Joyful", nil)
@@ -15,6 +9,23 @@ describe '#Word' do
       word2 = Word.new("Gloomy", nil)
       word2.save()
       expect(Word.all).to(eq([word, word2]))
+    end
+  end
+
+  describe('.clear') do
+    it("clears all words") do
+      word = Word.new("Joyful", nil)
+      word.save()
+      word2 = Word.new("Gloomy", nil)
+      word2.save()
+      Word.clear()
+      expect(Word.all).to(eq([]))
+    end
+  end
+
+  describe('.all') do
+    it("returns an empty array when there are no words") do
+      expect(Word.all).to(eq([]))
     end
   end
 

5a1f7a00de774e034a105908ee9e9b34edda47ce
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-05 18:15:16 -0700

    wrote method for save & word to compare - PASSES TESTS

--- lib/word.rb
+++ lib/word.rb
@@ -1,17 +1,26 @@
 require('pry')
 
 class Word
-  attr_reader :id, :name
+  attr_reader :id, :word
 
   @@words = {}
   @@total_rows = 0
 
   def initialize(word, id)
     @word = word
+    @id = id || @@total_rows += 1
   end
 
   def self.all
     @@words.values()
+  end
+
+  def save
+    @@words[self.id] = Word.new(self.word, self.id)
+  end
+
+  def ==(word_to_compare)
+    self.word() == word_to_compare.word()
   end
 
 end

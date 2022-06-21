20edf1f95d3fa42b9893129bcdb80742a12e58f1
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-21 15:54:07 -0700

    add testing for edit, update, delete - passes test

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -23,3 +23,51 @@ describe('create a definition path', {:type => :feature}) do
     expect(page).to have_content('A warship with a streamlined hull designed to operate completely submerged in the sea for long periods, equipped with a periscope and typically armed with torpedoes or missiles.')
   end
 end
+
+describe('edit a word', {:type => :feature}) do
+  it("edits a word and returns to word page") do
+    word = Word.new("Submarine", nil)
+    word.save()
+    visit("/words/#{word.id}")
+    click_on("Edit word")
+    fill_in("Update word", :with => "Spaceship")
+    click_on("Update!")
+    expect(page).to have_content("Spaceship")
+  end
+end
+
+describe('edit a definition', {:type => :feature}) do
+  it("edits a words definition and returns to word page") do
+    word = Word.new("Spaceship", nil)
+    word.save()
+    definition = Define.new("a spacecraft, especially one controlled by a crew.", word.id, nil)
+    definition.save()
+    visit("/words/#{word.id}/definition/#{definition.id}")
+    fill_in("Edit definition", :with => "Alien transportation")
+    click_on("Update definition")
+    expect(page).to have_content("Alien transportation")
+  end
+end
+
+describe('delete a word', {:type => :feature}) do
+  it("deletes a word and returns to word page") do
+    word = Word.new("Spaceship", nil)
+    word.save()
+    visit("/words/#{word.id}")
+    click_on("Edit word")
+    click_on("Delete!")
+    expect(page).to have_content("")
+  end
+end
+
+describe("deletes a definition", { :type => :feature }) do
+  it("delete a definition and return to word page") do
+    word = Word.new("Spaceship", nil)
+    word.save()
+    definition = Define.new("Alien transportation", word.id, nil)
+    definition.save()
+    visit("/words/#{word.id}/definition/#{definition.id}")
+    click_on("Delete definition")
+    expect(page).to have_content("")
+  end
+end
